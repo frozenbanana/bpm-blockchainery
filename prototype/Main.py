56b67f20f8c5350c8fb96be7e35a1b44887c86b0
CAJan93 <jan.mensch@gmx.net>
2018-12-04 15:36:33 +0100

    add ring

--- prototype/Main.py
+++ prototype/Main.py
@@ -1,11 +1,14 @@
 import random
 from Client import client
 from Blockchain import storage_on_chain
+import ring
+import Crypto.PublicKey.RSA
+import os
 
 """
 This file will simulate the clients and the blockchain 
 """
-
+"""
 storage = storage_on_chain()
 # load clients, if not exist
 
@@ -23,3 +26,26 @@ encrypted = a.encrypt_synch(msg)
 clear = a.decrypt_synch(encrypted)
 if clear == msg:
     print('sync encryption works!')
+"""
+
+
+def _rn(_):
+    return Crypto.PublicKey.RSA.generate(1024, os.urandom)
+
+
+size = 4
+message = "Hello"
+
+print "Message is:", message
+key = map(_rn, range(size))
+
+r = ring.ring(key)
+
+for i in range(size):
+    s1 = r.sign(message, i)
+# s2 = r.sign(msg2, i)
+    if (i == 1):
+        print "Signature is", s1
+        print "Signature verified:", r.verify(message, s1)
+
+#    assert r.verify(msg1, s1) and r.verify(msg2, s2) and not r.verify(msg1, s2)
