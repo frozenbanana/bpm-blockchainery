bfc2deb026be1c0391e0d0d595c3d05948162d49
CAJan93 <jan.mensch@gmx.net>
2018-12-05 09:20:17 +0100

    implement simple file communication

--- prototype/Client.py
+++ prototype/Client.py
@@ -1,12 +1,17 @@
 import uuid
+import os
+import time
+import threading
 from Crypto import Random
 from Crypto.PublicKey import RSA
+
 
 class client:
     """
     simulate a client
     """
-    def __init__(self, name): #, Chain):
+
+    def __init__(self, name):  # , Chain):
         self.name = name
         self.private_key, self.public_key = client.get_keys()
         self.sync_key = False
@@ -15,7 +20,8 @@ class client:
         # self.post_to_chain(self.id, self.public_key)
         # self.chain.add_user(self.id, self.public_key)
         # self.all_users = {}
-    
+        self.inbox = None
+
     @staticmethod
     def get_sync_key():
         # replace this with something smart
@@ -28,11 +34,60 @@ class client:
         public_key = new_key.publickey().exportKey("PEM")
         private_key = new_key.exportKey("PEM")
         return private_key, public_key
-    
 
     def __repr__(self):
         return "This is The client {} with the public key {}".format(self.name, self.public_key)
 
+    def make_receiver(self):
+        """
+        make client be able to receive messages
+        """
+        if not self.inbox:
+            self.inbox = os.path.join(os.path.dirname(
+                os.path.abspath(__file__)), self.name + '.txt')
+        #with open(self.inbox, 'w') as f:
+         #   pass
+
+    def make_receive_message(self, callback, frequency):
+        """
+        create a handler for a received message
+        @param callback: the function to call on receive message
+        @param frequency: check interval in seconds
+        """
+        def receive_message():
+            while True:
+                msg = ''
+                try:
+                    with open(self.inbox, 'r') as f:
+                        msg = f.read()
+                except:
+                    pass
+                if msg != '':
+                    # clear inbox
+                    callback(msg)
+                    with open(self.inbox, 'w') as f:
+                        pass
+                    msg == ''
+                time.sleep(frequency)
+        thread = threading.Thread(target=receive_message, args=())
+        thread.daemon = True                            # Daemonize thread
+        thread.start()
+        print('registered callback function on receive message')
+
+    def send_to(self, receiver, message):
+        """
+        send to a other client
+        @param receiver: the other client
+        @param message: the message send to the receiver
+        """
+        success = False
+        while not success:
+            try:
+                with open(os.path.join(os.path.dirname(os.path.abspath(__file__)), receiver + '.txt'), 'w') as f:
+                    f.write(message)
+                success = True
+            except:
+                time.sleep(5)
 
     def encrypt_synch(self, msg):
         if not self.sync_key:
@@ -52,20 +107,23 @@ class client:
 
     def post_to_chain(self, key, val):
         raise NotImplementedError
-        self.chain.add_to_storage(key, val)
+        #self.chain.add_to_storage(key, val)
 
     def get_from_chain(self, key):
         raise NotImplementedError
-        return self.chain[key]
+        # return self.chain[key]
 
     def get_all_users(self):
         raise NotImplementedError
-        return self.chain.users
+        # return self.chain.users
 
     def post_sync_key(self):
         raise NotImplementedError
+        """
         if self.master:
             self.all_users = self.get_all_users()
             for public_key in self.all_users.values():
                 public_key_sync = public_key.encrypt(self.sync_key)
-                self.chain.add_to_storage('user-' + public_key, public_key_sync)
+                self.chain.add_to_storage(
+                    'user-' + public_key, public_key_sync)
+        """
--- prototype/main.py
+++ prototype/main.py
@@ -5,27 +5,45 @@ import ring_signature
 import Crypto.PublicKey.RSA
 import os
 import argparse
+import socket
+import time
 
 
-def main(args):
+def callback_print(message):
+    print('received message: {}'.format(message))
+
+
+def main(args, possible_roles):
     """
     start a client with arguments specified by user
+    client will connect to localhost:(1234 + client_name_index)
     """
 
     try:
         cli = client(args.role)
     except Exception as e:
-        print "client creation failed with exception {}. Arguments were {}".format(
-            e.message, args)
+        print('client creation failed with exception {}. Arguments were {}'.format(e.message, args))
 
-    print "created the client {}".format(cli.name)
+    cli.make_receiver()
+
+    cli.make_receive_message(callback_print, 3)
+
+    if cli.name == 'buyer':
+        cli.send_to('seller', 'hi from buyer')
+    else:
+        cli.send_to('buyer', 'hi from seller')
+    
+    while True: 
+        time.sleep(1)
+
+    print("created the client {}".format(cli.name))
 
     size = args.num_clients
-    print "expecting a total number of {} clients participating in the ring signature".format(
-        size)
+    print("expecting a total number of {} clients participating in the ring signature".format(
+        size))
     message = "Hello"
 
-    print "Message is:", message
+    print("Message is:", message)
     key = map(ring_signature.ring.rn, range(size))
 
     r = ring_signature.ring(key)
@@ -33,10 +51,11 @@ def main(args):
     for i in range(size):
         s1 = r.sign(message, i)
         if (i == 1):
-            print "Signature is", s1
-            print "Signature verified:", r.verify(message, s1)
+            print("Signature is", s1)
+            print("Signature verified:", r.verify(message, s1))
 
     #    assert r.verify(msg1, s1) and r.verify(msg2, s2) and not r.verify(msg1, s2)
+    
 
 
 if __name__ == "__main__":
@@ -53,8 +72,10 @@ if __name__ == "__main__":
                             ', '.join(possible_roles)),
                         required=True)
 
+    """
     parser.add_argument('--num_clients', type=int,
                         help='the total number of clients involved in the communication',
                         required=True)
+    """
 
-    main(parser.parse_args())
+    main(parser.parse_args(), possible_roles)
